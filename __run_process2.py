16975f8ed93c7f43d8fbe6a3f389eba4b0862e5d
Thomas Cassady <thomas.cassady@littlefish.co.uk>
2020-01-09 17:06:12 +0000

    turning off update process so I can refresh tables

--- __run_process2.py
+++ __run_process2.py
@@ -6,10 +6,10 @@ exec(open("process\\process2\\_main2.py").read())
 
 output_database = 'LF-SQL-WH'
 
-#setup_warehouse2(output_database)
-#setup_warehouse_lflive2(output_database)
-#setup_warehouse_telephony2(output_database)
-#setup_warehouse_problem2(output_database)
+setup_warehouse2(output_database)
+setup_warehouse_lflive2(output_database)
+setup_warehouse_telephony2(output_database)
+setup_warehouse_problem2(output_database)
 
 update_warehouse_data_tables2()
 
